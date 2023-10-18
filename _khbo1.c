ae1b1cc57aa1eef374cded65fcca685565cdfbfe
Abdelrahman388 <abdelrahmansameh092@gmail.com>
2023-10-18 12:52:27 -0700

    fx _khbo1.c

--- _khbo1.c
+++ _khbo1.c
@@ -8,36 +8,36 @@
  */
 int custom_string_length(char *custom_str)
 {
-    int custom_i = 0;
+	int custom_i = 0;
 
-    if (!custom_str)
-        return (0);
+	if (!custom_str)
+		return (0);
 
-    while (*custom_str++)
-        custom_i++;
-    return custom_i;
+	while (*custom_str++)
+		custom_i++;
+	return (custom_i);
 }
 
 /**
- * custom_string_compare - performs lexicographic comparison of two custom strings.
+ * custom_string_compare - performs lexicographic comparison of two c strings.
  * @custom_str1: the first custom string
  * @custom_str2: the second custom string
  *
- * Return: negative if custom_str1 < custom_str2, positive if custom_str1 > custom_str2, zero if custom_str1 == custom_str2
+ * Return:-1 if c_str1 < c_str2,+1 if c_str1 > c_str2, 0 if c_str1 == c_str2
  */
 int custom_string_compare(char *custom_str1, char *custom_str2)
 {
-    while (*custom_str1 && *custom_str2)
-    {
-        if (*custom_str1 != *custom_str2)
-            return (*custom_str1 - *custom_str2);
-        custom_str1++;
-        custom_str2++;
-    }
-    if (*custom_str1 == *custom_str2)
-        return 0;
-    else
-        return (*custom_str1 < *custom_str2 ? -1 : 1);
+	while (*custom_str1 && *custom_str2)
+	{
+		if (*custom_str1 != *custom_str2)
+			return (*custom_str1 - *custom_str2);
+		custom_str1++;
+		custom_str2++;
+	}
+	if (*custom_str1 == *custom_str2)
+		return (0);
+	else
+		return (*custom_str1 < *custom_str2 ? -1 : 1);
 }
 
 /**
@@ -49,10 +49,10 @@ int custom_string_compare(char *custom_str1, char *custom_str2)
  */
 char *custom_starts_with(const char *custom_haystack, const char *custom_needle)
 {
-    while (*custom_needle)
-        if (*custom_needle++ != *custom_haystack++)
-            return NULL;
-    return (char *)custom_haystack;
+	while (*custom_needle)
+		if (*custom_needle++ != *custom_haystack++)
+			return (NULL);
+		return ((char *)custom_haystack);
 }
 
 /**
@@ -64,12 +64,12 @@ char *custom_starts_with(const char *custom_haystack, const char *custom_needle)
  */
 char *custom_string_concatenate(char *custom_dest, char *custom_src)
 {
-    char *custom_ret = custom_dest;
+	char *custom_ret = custom_dest;
 
-    while (*custom_dest)
-        custom_dest++;
-    while (*custom_src)
-        *custom_dest++ = *custom_src++;
-    *custom_dest = *custom_src;
-    return custom_ret;
+	while (*custom_dest)
+		custom_dest++;
+	while (*custom_src)
+		*custom_dest++ = *custom_src++;
+	*custom_dest = *custom_src;
+	return (custom_ret);
 }
